9051778dd53abab9510cbf1a0334bbcd24344645
Heilmann, Quentin (DI PA MF-H MI-PV MP-4) <quentin.heilmann@siemens.com>
2024-12-06 06:12:52 +0100

    add podcasts, home page

--- frontend/src/app/pages/podcasts/podcasts.component.ts
+++ frontend/src/app/pages/podcasts/podcasts.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-podcasts',
-  standalone: true,
-  imports: [],
-  templateUrl: './podcasts.component.html',
-  styleUrl: './podcasts.component.less'
-})
-export class PodcastsComponent {
-
-}
+// N'est plus utilisé, mais conservé pour référence.
