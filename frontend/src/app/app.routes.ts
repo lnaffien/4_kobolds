31e9e8f2fbccc88fa4a97d48e9430f081d8d2f47
Heilmann, Quentin (DI PA MF-H MI-PV MP-4) <quentin.heilmann@siemens.com>
2024-12-06 03:16:38 +0100

    feat(ui) sidebar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import { GameComponent } from '../game/game.component'; // Correct path for the GameComponent
+import { GameComponent } from '../game/game.component';
+import { PodcastsComponent } from './pages/podcasts/podcasts.component';
+import { CreditsComponent } from './pages/credits/credits.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
-  { path: 'game', pathMatch: 'full', component: GameComponent }
+  { path: 'game', pathMatch: 'full', component: GameComponent },
+  { path: 'podcasts', pathMatch: 'full', component: PodcastsComponent },
+  { path: 'credits', pathMatch: 'full', component: CreditsComponent }
 ];
