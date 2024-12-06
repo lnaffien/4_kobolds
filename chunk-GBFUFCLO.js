7a1266030779a9ff109c38ada52617de78908161
Heilmann, Quentin (DI PA MF-H MI-PV MP-4) <quentin.heilmann@siemens.com>
2024-12-06 07:38:22 +0100

    Auto-generated commit

---
+++ chunk-GBFUFCLO.js
@@ -0,0 +1,95 @@
+import {
+  ɵsetClassDebugInfo,
+  ɵɵStandaloneFeature,
+  ɵɵdefineComponent,
+  ɵɵelement,
+  ɵɵelementEnd,
+  ɵɵelementStart,
+  ɵɵtext
+} from "./chunk-5ID6OIEU.js";
+
+// src/app/pages/home/home.component.ts
+var HomeComponent = class _HomeComponent {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+  static {
+    this.\u0275fac = function HomeComponent_Factory(__ngFactoryType__) {
+      return new (__ngFactoryType__ || _HomeComponent)();
+    };
+  }
+  static {
+    this.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _HomeComponent, selectors: [["app-home"]], standalone: true, features: [\u0275\u0275StandaloneFeature], decls: 46, vars: 0, consts: [[1, "homepage"], [1, "content-container"], [1, "header"], [1, "introduction"], [1, "objective"], [1, "association"], ["href", "https://www.raceforwater.org/en/", "target", "_blank"], [1, "inspiring-content"], [1, "media-links"], ["controls", ""], ["src", "/assets/siteui/PODCAST-Florian Se\u0301vellec.m4a", "type", "audio/mpeg"], ["href", "https://we.tl/t-OIFJXjnkko", "target", "_blank"]], template: function HomeComponent_Template(rf, ctx) {
+      if (rf & 1) {
+        \u0275\u0275elementStart(0, "div", 0)(1, "div", 1)(2, "header", 2)(3, "h1");
+        \u0275\u0275text(4, "La Nuit de l'Info 2024");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(5, "p");
+        \u0275\u0275text(6, "Nous sommes pr\xEAts pour relever le d\xE9fi !");
+        \u0275\u0275elementEnd()();
+        \u0275\u0275elementStart(7, "section", 3)(8, "h2");
+        \u0275\u0275text(9, "Qui sommes-nous ?");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(10, "p");
+        \u0275\u0275text(11, "Nous sommes les 4 Kobolds, un groupe de passionn\xE9s orient\xE9 informatique industrielle, syst\xE8me num\xE9rique, \xE9lectronique et innovation num\xE9rique, pr\xEAts \xE0 relever les d\xE9fis lanc\xE9s lors de La Nuit de l'Info !");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(12, "p");
+        \u0275\u0275text(13, "Cette comp\xE9tition nationale rassemble \xE9tudiants, enseignants et entreprises pour collaborer autour de la cr\xE9ation d\u2019une application Web 2.0, dans une ambiance stimulante et cr\xE9ative.");
+        \u0275\u0275elementEnd()();
+        \u0275\u0275elementStart(14, "section", 4)(15, "h2");
+        \u0275\u0275text(16, "Notre Objectif");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(17, "p");
+        \u0275\u0275text(18, "Durant cette nuit exceptionnelle, notre objectif est de concevoir, d\xE9velopper et pr\xE9senter une application web innovante qui r\xE9pond aux d\xE9fis propos\xE9s par les partenaires de l'\xE9v\xE9nement. Nous souhaitons cr\xE9er une application qui se distingue par son ergonomie, son architecture et la qualit\xE9 de la collaboration au sein de notre \xE9quipe.");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(19, "p");
+        \u0275\u0275text(20, "Pour atteindre cet objectif, nous allons combiner nos comp\xE9tences vari\xE9es, notre cr\xE9ativit\xE9 et notre motivation. Nous voulons non seulement relever les d\xE9fis techniques, mais aussi proposer une solution qui soit intuitive et agr\xE9able \xE0 utiliser pour les utilisateurs finaux. Nous croyons fermement que la synergie de notre \xE9quipe nous permettra de surmonter les obstacles et de produire un r\xE9sultat dont nous serons fiers.");
+        \u0275\u0275elementEnd()();
+        \u0275\u0275elementStart(21, "section", 5)(22, "h2");
+        \u0275\u0275text(23, "L'association");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(24, "p");
+        \u0275\u0275text(25, "Pour cette \xE9dition de La Nuit de l'Info, \xE0 l'honneur de Race for Water, une fondation qui lutte pour la pr\xE9servation des oc\xE9ans et agit contre la pollution plastique. Leur mission est essentielle : prot\xE9ger une ressource vitale pour notre plan\xE8te tout en d\xE9veloppant des solutions innovantes et durables.");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(26, "p");
+        \u0275\u0275text(27, "En int\xE9grant leur vision, nous souhaitons que notre projet refl\xE8te les valeurs de respect de l\u2019environnement et d\u2019impact positif. Ensemble, nous croyons qu\u2019il est possible de construire un futur plus propre et responsable.");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(28, "p");
+        \u0275\u0275text(29, "D\xE9couvrez-en plus sur ");
+        \u0275\u0275elementStart(30, "a", 6);
+        \u0275\u0275text(31, "Race for Water");
+        \u0275\u0275elementEnd()()();
+        \u0275\u0275elementStart(32, "section", 7)(33, "h2");
+        \u0275\u0275text(34, "Un contenu inspirant : Podcast/Vid\xE9o Race for Water");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(35, "p");
+        \u0275\u0275text(36, 'Pour mieux comprendre l\u2019enjeu de leur combat, nous vous invitons \xE0 \xE9couter le podcast "L\u2019urgence des oc\xE9ans : Agir maintenant", o\xF9 les membres de Race for Water partagent leur expertise et leur vision pour un monde sans plastique dans les mers. Ce contenu offre une r\xE9flexion passionnante sur les actions concr\xE8tes que chacun peut mener.');
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(37, "div", 8)(38, "p");
+        \u0275\u0275text(39, "\u{1F3A7} \xC9coutez le podcast ici :");
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(40, "audio", 9);
+        \u0275\u0275element(41, "source", 10);
+        \u0275\u0275elementEnd();
+        \u0275\u0275elementStart(42, "p");
+        \u0275\u0275text(43, "\u{1F4F9} Regardez la vid\xE9o ici : ");
+        \u0275\u0275elementStart(44, "a", 11);
+        \u0275\u0275text(45, "Lien vid\xE9o");
+        \u0275\u0275elementEnd()()()()()();
+      }
+    }, styles: ["\n\n.homepage[_ngcontent-%COMP%] {\n  font-family: Arial, sans-serif;\n  margin: 20px;\n  height: calc(100vh - 200px);\n  overflow: hidden;\n}\n.content-container[_ngcontent-%COMP%] {\n  height: calc(100vh);\n  overflow-y: scroll;\n  padding-right: 10px;\n}\n.content-container[_ngcontent-%COMP%]::-webkit-scrollbar {\n  display: none;\n}\n.content-container[_ngcontent-%COMP%] {\n  -ms-overflow-style: none;\n  scrollbar-width: none;\n}\n.header[_ngcontent-%COMP%] {\n  text-align: center;\n  background-color: #f4f4f4;\n  padding: 20px;\n  border-radius: 10px;\n}\n.introduction[_ngcontent-%COMP%], \n.objective[_ngcontent-%COMP%], \n.association[_ngcontent-%COMP%], \n.inspiring-content[_ngcontent-%COMP%] {\n  margin: 20px 0;\n}\n.media-links[_ngcontent-%COMP%] {\n  margin-top: 10px;\n}\n.footer[_ngcontent-%COMP%] {\n  text-align: center;\n  margin-top: 20px;\n}\n.inspiring-content[_ngcontent-%COMP%] {\n  margin-bottom: 15em;\n}\naudio[_ngcontent-%COMP%] {\n  background-color: blue;\n  width: 70%;\n}\n/*# sourceMappingURL=https://lnaffien.github.io/4_kobolds/home.component.css.map */"] });
+  }
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && \u0275setClassDebugInfo(HomeComponent, { className: "HomeComponent", filePath: "src\\app\\pages\\home\\home.component.ts", lineNumber: 9 });
+})();
+
+// src/app/pages/home/home.routes.ts
+var HOME_ROUTES = [
+  { path: "", component: HomeComponent }
+];
+export {
+  HOME_ROUTES
+};
+//# sourceMappingURL=chunk-GBFUFCLO.js.map
